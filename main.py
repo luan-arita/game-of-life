8c396856f66cab925d2a929ebb05d9f152ad18a3
Luan Arita <luan.arita@unesp.br>
2024-03-06 17:18:17 -0300

    Added Langton's Ant function.

--- main.py
+++ main.py
@@ -91,15 +91,74 @@ def run_forever(init_state):
         im.set_array(init_state)
         return im,
 
-    anim = FuncAnimation(fig, update, frames = 50, interval = 1000/10, blit = True)
+    anim = FuncAnimation(fig, update, frames = 50, interval = 1000/60, blit = True)
 
     return anim
 
+def move_ant(board, ant_position, ant_direction):
+
+    if ant_direction == 'up':
+        ant_position[1] += 1
+    elif ant_direction == 'right':
+        ant_position[0] += 1
+    elif ant_direction == 'down':
+        ant_position[1] -= 1
+    elif ant_direction == 'left':
+        ant_position[0] -= 1
+    
+    if board[ant_position[0]][ant_position[1]] == 0:
+        board[ant_position[0]][ant_position[1]] = 1
+        if ant_direction == 'up':
+            ant_direction = 'right'
+        elif ant_direction == 'right':
+            ant_direction = 'down'
+        elif ant_direction == 'down':
+            ant_direction = 'left'
+        elif ant_direction == 'left':
+            ant_direction = 'up'
+
+    else:
+        board[ant_position[0]][ant_position[1]] = 0
+        if ant_direction == 'up':
+            ant_direction = 'left'
+        elif ant_direction == 'left':
+            ant_direction = 'down'
+        elif ant_direction == 'down':
+            ant_direction = 'right'
+        elif ant_direction == 'right':
+            ant_direction = 'up'
+    return ant_direction
+
+
+ant_direction = 'up'
+
+def run_langton(board, ant_position):
+    
+    fig = plt.figure()
+    ax = fig.add_subplot(111)
+    im = plt.imshow(board, interpolation='none', cmap = plt.cm.gray, vmin=0, vmax=1)
+    
+    def animate(frame, ant_position):
+        global ant_direction
+        ant_direction = move_ant(board, ant_position, ant_direction)
+        im.set_array(board)
+
+        return im,
+
+    anim = FuncAnimation(fig, animate, frames = 50, interval = 0.5, blit = True, fargs = (ant_position,))
+
+    return anim
+
+
 def start_animation():
-    board = random_state(100, 100)
+    #board = random_state(200, 200)
+    board = dead_state(100, 100)
+    width = len(board[0])
+    height = len(board)
+    ant_position = [width // 2, height // 2]
     #board = load_board_state("pulsar.txt")
-    anim = run_forever(board)
+    #anim = run_forever(board)
+    anim2 = run_langton(board, ant_position)
     plt.show()
 
 start_animation()
-
